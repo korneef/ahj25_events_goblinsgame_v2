651faa41b4c9233ab9decee98d9be38ef3c59a14
Valentin Korneev <v.korneef@gmail.com>
2022-10-28 16:31:22 +0600

    homework v2.1

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,9 +1,7 @@
-import GameClass from '../gameClass'
-import gameaa from '../game'
-import enemy from '../../img/goblin.png'
+import GameClass from '../gameClass';
 
 test('randomNumberFunction', () => {
-  const game = new GameClass;
+  const game = new GameClass();
   game.randomNumber(1, 3);
   const received = game.random;
   const expected = [1, 2, 3].find((item) => {
